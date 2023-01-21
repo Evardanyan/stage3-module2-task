276db750dddaf6f7d7571e74c046ab4a84e416d4
Edgar Vardanyan <e.vardan4an@gmail.com>
2023-01-21 04:33:44 +0400

    added get authors method in DataSource.java

--- module-repository/src/main/java/com/mjc/school/repository/utils/DataSource.java
+++ module-repository/src/main/java/com/mjc/school/repository/utils/DataSource.java
@@ -1,6 +1,8 @@
 package com.mjc.school.repository.utils;
 
 
+import com.mjc.school.repository.model.data.AuthorData;
+import com.mjc.school.repository.model.data.AuthorModel;
 import com.mjc.school.repository.model.data.NewsData;
 import com.mjc.school.repository.model.data.NewsModel;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,15 +19,25 @@ public class DataSource {
     @Autowired
     private NewsData newsData;
 
+    @Autowired
+    private AuthorData authorData;
+
     private List<NewsModel> news;
+    private List<AuthorModel> authors;
+
 
     @PostConstruct
     private void init() {
        news =  newsData.getNewsList();
+       authors = authorData.getAuthorList();
     }
 
     public List<NewsModel> getNews() {
         return this.news;
+    }
+
+    public List<AuthorModel> getAuthors() {
+        return this.authors;
     }
 
 }
