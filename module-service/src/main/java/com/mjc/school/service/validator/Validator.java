cdceed533d24e4c19ed0381c215c93b57e3e11e9
Edgar Vardanyan <e.vardan4an@gmail.com>
2023-01-25 21:38:39 +0400

    minor changes in Validator.java

--- module-service/src/main/java/com/mjc/school/service/validator/Validator.java
+++ module-service/src/main/java/com/mjc/school/service/validator/Validator.java
@@ -1,39 +1,36 @@
 package com.mjc.school.service.validator;
 
+import com.mjc.school.repository.utils.DataSource;
 import com.mjc.school.service.dto.AuthorDtoRequest;
 import com.mjc.school.service.dto.NewsDtoRequest;
 import com.mjc.school.service.exception.ServiceErrorCodeMessage;
 import com.mjc.school.service.exception.ValidatorException;
+import com.mjc.school.service.impl.AuthorService;
+import com.mjc.school.service.impl.NewsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Validator {
-    private static Validator newsValidator;
-    private static final String NEWS_ID = "News id";
-    private static final String NEWS_CONTENT = "News content";
-    private static final String AUTHOR_ID = "Author id";
-    private static final String NEWS_TITLE = "News title";
-    private static final Integer NEWS_CONTENT_MIN_LENGTH = 5;;
-    private static final Integer NEWS_CONTENT_MAX_LENGTH = 255;
-    private static final Integer NEWS_TITLE_MIN_LENGTH = 5;
-    private static final Integer NEWS_TITLE_MAX_LENGTH = 30;
-    private static final Integer AUTHOR_NAME_LENGTH_MIN = 3;
-    private static final Integer AUTHOR_NAME_LENGTH_MAX = 15;
-    private static final Integer MAX_AUTHOR_ID = 20;
+
+    private final String NEWS_ID = "News id";
+    private final String NEWS_CONTENT = "News content";
+    private final String AUTHOR_ID = "Author id";
+    private final String NEWS_TITLE = "News title";
+    private final Integer NEWS_CONTENT_MIN_LENGTH = 5;;
+    private final Integer NEWS_CONTENT_MAX_LENGTH = 255;
+    private final Integer NEWS_TITLE_MIN_LENGTH = 5;
+    private final Integer NEWS_TITLE_MAX_LENGTH = 30;
+    private final Integer AUTHOR_NAME_LENGTH_MIN = 3;
+    private final Integer AUTHOR_NAME_LENGTH_MAX = 15;
 
 
     public void validateNewsId(Long newsId) {
         this.validateNumber(newsId, NEWS_ID);
-        if (newsId > (long)MAX_AUTHOR_ID.intValue()) {
-            throw new ValidatorException(String.format(ServiceErrorCodeMessage.NEWS_ID_DOES_NOT_EXIST.getCodeMsg(), newsId));
-        }
     }
 
     public void validateAuthorId(Long authorId) {
         this.validateNumber(authorId, AUTHOR_ID);
-        if (authorId > (long)MAX_AUTHOR_ID.intValue()) {
-            throw new ValidatorException(String.format(ServiceErrorCodeMessage.AUTHOR_ID_DOES_NOT_EXIST.getCodeMsg(), authorId));
-        }
     }
 
     public void validateNewsDto(NewsDtoRequest dtoRequest) {
@@ -68,14 +65,4 @@ public class Validator {
             throw new ValidatorException(String.format(ServiceErrorCodeMessage.VALIDATE_STRING_LENGTH.getCodeMsg(), parameter, minLength, maxLength, parameter, value));
         }
     }
-
-//    static {
-//        NEWS_CONTENT_MIN_LENGTH = 5;
-//        NEWS_CONTENT_MAX_LENGTH = 255;
-//        AUTHOR_NAME_LENGTH_MIN = 3;
-//        AUTHOR_NAME_LENGTH_MAX = 15;
-//        NEWS_TITLE_MIN_LENGTH = 5;
-//        NEWS_TITLE_MAX_LENGTH = 30;
-//        MAX_AUTHOR_ID = 20;
-//    }
 }
